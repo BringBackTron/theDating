e187afe320fabc56fee528dd9aff5969c06f54ce
George McMullen <gmcmullen@mail.greenriver.edu>
2021-03-26 08:20:22 -0400

    Created UI for Info, Profile, and interests pages

--- index.php
+++ index.php
@@ -24,11 +24,45 @@ $f3->route('GET /', function(){
     echo $view->render('views/home.html');
 });
 
-//Define a default route (login page)
-$f3->route('GET /login', function(){
-    // render home.html
+//Define an order route
+$f3->route('GET|POST /info', function($f3) {
+    // Display a view
     $view = new Template();
-    echo $view->render('views/login.html');
+    echo $view->render("views/info.html");
+});
+
+//Define an order2 route
+$f3->route('POST /profile', function($f3) {
+    // Display a view
+    $view = new Template();
+    echo $view->render("views/profile.html");
+});
+
+//Define an order2 route
+$f3->route('POST /interests', function($f3) {
+    // Display a view
+    $view = new Template();
+    echo $view->render("views/interests.html");
+});
+
+//Define a summary route
+$f3->route('POST /summary', function() {
+    //var_dump($_POST);
+    //echo "<br>";
+
+    /* Add data from form2 to Session array
+    if (!empty($_POST['name'])) {
+        $_SESSION['name'] = $_POST['name'];
+    }
+    $_SESSION['size'] = $_POST['size'];
+    $_SESSION['accessories'] = implode(", ", $_POST['accessories']);
+    //var_dump($_SESSION);
+
+    */
+
+    // Display a view
+    $view = new Template();
+    echo $view->render("views/summary.html");
 });
 
 
